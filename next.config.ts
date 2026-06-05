45508c711c196a670e7ed42a3ff57a38c5b0f0ce
PantaAastha <ap.pantaaastha@gmail.com>
2026-06-05 17:06:02 +0545

    Minor update

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
-import type { NextConfig } from "next";
-
-const isProd = process.env.NODE_ENV === "production";
-// GitHub Pages hosts project sites at username.github.io/repo-name
-// If GITHUB_REPOSITORY is present, we extract the repo-name to use as basePath.
-// Note: If you configure a custom domain, you should disable/remove this basePath check.
-const basePath = isProd && process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}` : "";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
-  basePath: basePath,
-  images: {
-    unoptimized: true,
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
